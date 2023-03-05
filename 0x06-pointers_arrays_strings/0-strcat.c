4e913295e8be336e7ea8b61188c6f233f54bad7e
elkomysara <sa1244@fayoum.edu.eg>
2023-03-04 23:08:26 -0800

    mesage

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++]
-	return (dest);
+	int i, c;
+	
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (c = 0; src[c] != '\0'; c++)
+	{
+		dest[i] = src[c];
+		i++;
+	}
+	dest[i] = '\0';
+	return(dest);
 }
