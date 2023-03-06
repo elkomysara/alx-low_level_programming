4e1fb9aee766916097a2eaf82272b0e6e4abf773
elkomysara <sa1244@fayoum.edu.eg>
2023-03-05 23:12:40 -0800

    mesage

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,14 @@
 #include "main.c"
 
-int_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	while(*s1 && *s2 && *s1 == *s2)
+	int i;
 
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+
 	}
-	return (*s1 - *s2);
+	return (0);
 }
