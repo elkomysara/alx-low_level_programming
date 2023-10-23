972a27d199e09aa0586433cf3c5d9661b77ac960
elkomysara <sa1244@fayoum.edu.eg>
2023-10-23 14:40:51 -0700

    place identifier

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-int main() {
-  int n;
+int main(void)
+{
+	int n;
 
-  n = rand() % RAND_MAX;
-
-  if (n > 0) {
-    printf("%d is positive\n", n);
-  } else if (n == 0) {
-    printf("%d is zero\n", n);
-  } else {
-    printf("%d is negative\n", n);
-  }
-
-  return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
