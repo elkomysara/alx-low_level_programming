a94d78dd22476b2ba9722769d013eb74d5ab10b9
elkomysara <sa1244@fayoum.edu.eg>
2023-10-23 14:32:49 -0700

    compiled

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,8 @@
 int main() {
   int n;
 
-  // Generate a random number
   n = rand() % RAND_MAX;
 
-  // Check if the number is positive or negative
   if (n > 0) {
     printf("%d is positive\n", n);
   } else if (n == 0) {
