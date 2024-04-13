7eb55c0d52004da436a8f3398b49f140d7d148a8
elkomysara <sa1244@fayoum.edu.eg>
2024-04-14 00:29:23 +0200

    0x01-variables_if_else_while/3-name

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 /**
@@ -12,6 +12,10 @@ int main(void)
 {
 int x;
 for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+for (x = 'A'; x <= 'Z'; x++)
 {
 putchar(x);
 }
