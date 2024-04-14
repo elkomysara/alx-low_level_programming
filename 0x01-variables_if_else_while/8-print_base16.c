859f0ace3a5e0cbc228d912b3feb0cbab09c3fea
elkomysara <sa1244@fayoum.edu.eg>
2024-04-15 00:02:22 +0200

    0x03-shell_variables_expansions/8-true_knowledge

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
 /**
- * main - prints all
- * the numbers of base 16 in lowercase,
- * followed by a new line
+ * main - prints all possible
+ * different combinations of two digits.
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 int x;
-for (x = 0; x <= 9; x++)
+int y;
+int z;
+
+y = 0;
+for (x = 0; x <= 7; x++)
+{
+for (y = 1; y <= 8; y++)
+{
+for (z = 2; z <= 9; z++)
+{
+if (z > y && y > x)
 {
 putchar(x + '0');
-}
-for (x = 'a'; x <= 'f'; x++)
+putchar(y + '0');
+putchar(z + '0');
+if (x < 7 || y < 8 || z < 9)
 {
-putchar(x);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
 }
 putchar('\n');
 return (0);
