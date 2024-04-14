bf15c2e51b65cdb2097a72ac84361bffe559c28c
elkomysara <sa1244@fayoum.edu.eg>
2024-04-14 22:48:53 +0200

    0x03-shell_variables_expansions/7-create_global_variable

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
