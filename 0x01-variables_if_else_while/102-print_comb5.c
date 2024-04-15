3db958ada2d31eadaa93cabe53ad78d47826776f
elkomysara <sa1244@fayoum.edu.eg>
2024-04-15 10:37:02 +0200

    0x03-shell_variables_expansions/8-true_knowledge

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all
+ * the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int x;
+int y;
+
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0')
+}
+putchar('\n');
+return (0);
+}
