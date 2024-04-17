d97edee5d5e467bdece937a43d08743f5e12a594
elkomysara <sa1244@fayoum.edu.eg>
2024-04-18 01:38:16 +0200

    0x02-functions_nested_loops/5-print_numbers.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* main - Entry point
-*
+* print_sign - Entry point
+* @x: parameter
 * Return: Always 0 (Success)
 */
 
@@ -10,17 +10,18 @@ int print_sign(int x)
 if (x > 0)
 {
 _putchar('+');
-return(1);
+return (1);
 }
 else if (x == 0)
 {
 _putchar(0 + '0');
-return(0);
+return (0);
 }
-else if ( x < 0)
+else if (x < 0)
 {
 _putchar('-');
-return(-1);
+
 }
+return (-1);
 }
 
