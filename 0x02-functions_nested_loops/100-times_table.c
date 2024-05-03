871840977ef5f4567eba34d26c9ba8ee0acb3fa4
elkomysara <sa1244@fayoum.edu.eg>
2024-05-03 20:10:56 +0300

    0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,10 +16,10 @@ if (n < 0 || n > 15)
 return;
 }
 
-for (i = 0; i <= n; ++i)
+for (i = 0; i <= n; i++)
 {
 int j;
-for (j = 0; j <= n; ++j)
+for (j = 0; j <= n; j++)
 {
 int result = i * j;
 if (j == 0)
