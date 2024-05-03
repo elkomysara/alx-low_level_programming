00dcbf16628e01a2418b902d5c3922126e06b19b
elkomysara <sa1244@fayoum.edu.eg>
2024-05-03 18:52:12 +0300

    0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,36 @@
+
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
+* print_times_table - prints the n times table, starting with 0
+* @n: print times table to this number
+*/
+
 void print_times_table(int n)
 {
-	int i, j, k;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int m;
+m = 0;
+
+while (m <= n && n <= 15 && n >= 0)
+{
+int k;
+k = 0;
+while (k <= n && n <= 15 && n >= 0)
+{
+if (k == 0)
+{
+printf("%d", k *m);
+}
+else
+{
+printf(",%d", k *m);
+}
+k++;
+}
+m++;
+printf("\n");
+}
+printf("\n");
 }
