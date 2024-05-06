11cdfb360ea45832299548a788a90f98e1118217
elkomysara <sa1244@fayoum.edu.eg>
2024-05-06 15:36:41 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers
+* @a: int to be multiplied
+* @b: int to be multiplied
+* Return: multiplication
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
