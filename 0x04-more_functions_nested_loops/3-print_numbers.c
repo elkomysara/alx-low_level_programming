634e7089df80412b0e152baabcd367284f715817
elkomysara <sa1244@fayoum.edu.eg>
2024-05-08 01:53:15 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
-* print_numbers - A function that prints the numbers, from 0 to 9 
-* followed by a new line.
-* @x: the counter
-*/
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
 
 void print_numbers(void)
 {
-int x =0;
-while (x < 10)
+int i;
+
+for (i = 0; i < 10; i++)
 {
-_putchar(x + '0');
-x++;
+_putchar(i + '0');
 }
+
 _putchar('\n');
 }
