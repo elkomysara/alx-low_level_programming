f8ad35656597c34cc3606148874906a8cdf90059
elkomysara <sa1244@fayoum.edu.eg>
2024-05-11 23:34:30 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio>
+
+/**
+ * main - FizzBuzz test
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i == 1)
+{
+printf('%d', i);
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+}
+printf("\n");
+}
