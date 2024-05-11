eb01eaaf85e2ef76ff879732f02b7bbc8189b41a
elkomysara <sa1244@fayoum.edu.eg>
2024-05-12 01:02:49 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,27 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* print_number - A function that prints integer
-* @n: is the number
+* print_number - prints an integer
+* @n: integer to be printed
 */
 void print_number(int n)
 {
-return (_putchar('n' + 0));
+unsigned int n1;
+
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+}
+else
+{
+n1 = n;
+}
+
+if (n1 / 10)
+{
+print_number(n1 / 10);
+}
+
+_putchar((n1 % 10) + '0');
 }
