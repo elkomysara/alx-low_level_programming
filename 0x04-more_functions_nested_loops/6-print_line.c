1d54c71a7c85f0c2bff0b77335b383eae4f0bfe3
elkomysara <sa1244@fayoum.edu.eg>
2024-05-11 18:01:39 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - draws a straight line in the terminal
+* @n: the number of times the character _ should be printed
+*/
+void print_line(int n)
+{
+int i = 1;
+if (n <= 0)
+{
+_putchar('\n');
+}
+while ( i <= n)
+{
+_putchar('_');
+i++;
+}
+}
