77f660625cd9a550bb8200ecfadb8609c01002e9
elkomysara <sa1244@fayoum.edu.eg>
2024-06-07 14:40:15 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -1,48 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * modif_my_char_var - modifies a char variable through a pointer
- *
- * Return: nothing.
- */
-void modif_my_char_var(char *cc, char ccc)
-{
-    printf("Inside modif_my_char_var - Address of 'cc': %p\n", (void*)&cc);
-    printf("Inside modif_my_char_var - Value of 'cc': %p\n", (void*)cc);
-    printf("Inside modif_my_char_var - Address of 'ccc': %p\n", (void*)&ccc);
-    printf("Inside modif_my_char_var - Value of 'ccc': %c\n", ccc);
-
-    *cc = 'o';
-    ccc = 'l';
-
-    printf("Inside modif_my_char_var - After modification - Value of '*cc': %c\n", *cc);
-    printf("Inside modif_my_char_var - After modification - Value of 'ccc': %c\n", ccc);
-}
-
-/**
- * main - testing the modification of char variables
+ * main - check the code for
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char c;
-    char *p;
+    int array[5];
 
-    p = &c;
-    c = 'H';
-
-    printf("Before modif_my_char_var - Value of 'c': %c\n", c);
-    printf("Before modif_my_char_var - Address of 'c': %p\n", (void*)&c);
-    printf("Before modif_my_char_var - Value of 'p': %p\n", (void*)p);
-    printf("Before modif_my_char_var - Address of 'p': %p\n", (void*)&p);
-
-    modif_my_char_var(p, c);
-
-    printf("After modif_my_char_var - Value of 'c': %c\n", c);
-    printf("After modif_my_char_var - Address of 'c': %p\n", (void*)&c);
-    printf("After modif_my_char_var - Value of 'p': %p\n", (void*)p);
-    printf("After modif_my_char_var - Address of 'p': %p\n", (void*)&p);
-
+    array[0] = 98;
+    array[1] = 402;
+    array[2] = -198;
+    array[3] = 298;
+    array[4] = -1024;
+    print_array(array, 5);
     return (0);
 }
