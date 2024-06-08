aebf77416617d23622a8f42ec9eb027b9dd51df9
elkomysara <sa1244@fayoum.edu.eg>
2024-06-08 20:45:09 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,26 +14,25 @@ int started = 0; /* Flag to indicate if conversion has started */
 
 while (*s != '\0') /* Loop through the string until the null terminator */
 {
-if (*s == '-' && !started)
-/* If a minus sign is found and conversion hasn't started */
+if (*s == '-' && !started) /* If a minus sign is found and conversion hasn't started */
 {
 sign *= -1; /* Flip the sign */
 }
-else if (*s >= '0' && *s <= '9')
-/* If a numeric character is found */
+else if (*s == '+' && !started) /* If a plus sign is found and conversion hasn't started */
+{
+/* Do nothing, just skip the sign */
+}
+else if (*s >= '0' && *s <= '9') /* If a numeric character is found */
 {
 started = 1; /* Indicate that conversion has started */
-result = result * 10 + (*s - '0');
-/* Convert character to number and add to result */
+result = result * 10 + (*s - '0'); /* Convert character to number and add to result */
 }
-else if (started)
-/* If a non-numeric character is found after conversion has started */
+else if (started) /* If a non-numeric character is found after conversion has started */
 {
 break; /* Stop conversion */
 }
 s++; /* Move to the next character */
 }
 
-return (result *sign);
-/* Apply the sign to the result and return */
+return result * sign; /* Apply the sign to the result and return */
 }
