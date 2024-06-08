54ed8669a686b2935d6dd609a502fd34c09b0e54
elkomysara <sa1244@fayoum.edu.eg>
2024-06-08 23:08:50 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,40 +12,31 @@ int _atoi(char *s)
 {
 int res = 0;
 int sign = 1;
-/* To handle negative numbers */
 int found_digit = 0;
-/* To track if a digit has been found */
-
-
-/* Skip leading whitespace */
-while (*s == ' ' || (*s >= '\t' && *s <= '\r')) {
+while (*s == ' ' || (*s >= '\t' && *s <= '\r'))
+{
 s++;
 }
-
-/* Check for sign */
 if (*s == '-' || *s == '+')
 {
 sign = (*s == '-') ? -1 : 1;
 s++;
 }
-
-/* Convert digits to integer */
 while (*s)
 {
 if (*s >= '0' && *s <= '9')
 {
-/* Check for overflow */
 if (res > INT_MAX / 10 || (res == INT_MAX / 10 && (*s - '0') > INT_MAX % 10))
 {
-return (sign == 1) ? INT_MAX : INT_MIN;
+return ((sign == 1) ? INT_MAX : INT_MIN);
 }
 res = res * 10 + (*s - '0');
 found_digit = 1;
 }
 else
 {
-/* Break if non-digit encountered after a digit */
-if (found_digit) {
+if (found_digit)
+{
 break;
 }
 }
