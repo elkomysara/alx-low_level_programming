701f7f0a63d1a25bbe4f3f7175be69bf7f82f319
elkomysara <sa1244@fayoum.edu.eg>
2024-06-09 02:11:16 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,33 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
-* main - generates a keygen
-*
-* Return: The integer value of the converted string
-*/
+#define PASSWORD_LENGTH 10
 
+int main() {
+    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int charset_size = sizeof(charset) - 1;
+    char password[PASSWORD_LENGTH + 1];
+    int i;
 
-int main(void)
-{
-int password_length; /* Example length */
-char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-int charset_size = sizeof(charset) - 1;
-char password[11]; /* password_length + 1 for null terminator */
-int i;
+    srand(time(NULL));
 
-srand(time(NULL)); /* Initialize random number generator */
-password_length = 10; /* Assign the password length */
+    /* Generate the password */
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        password[i] = charset[rand() % charset_size];
+    }
+    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the string
 
-/* Generate the password */
-for (i = 0; i < password_length; i++)
-{
-password[i] = charset[rand() % charset_size];
-}
-password[password_length] = '\0'; /* Null-terminate the string */
+    /* Print the generated password */
+    printf("%s\n", password);
 
-/* Print the generated password */
-printf("%s\n", password);
-
-return (0);
+    return 0;
 }
