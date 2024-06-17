9ab4923e21366f4dbcb0955f8395ac9d70d820d1
elkomysara <sa1244@fayoum.edu.eg>
2024-06-17 17:18:29 +0300

    alx-low_level_programming/0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: the string to be searched
+* @needle: the substring to be found
+*
+* Return: a pointer to the beginning of the located substring,
+*         or NULL if the substring is not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+char *h, *n;
+
+while (*haystack)
+{
+h = haystack;
+n = needle;
+
+while (*n && *h == *n)
+{
+h++;
+n++;
+}
+
+if (!*n)
+return (haystack);
+
+haystack++;
+}
+return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,11 +4,15 @@
 #include <stdio.h>
 
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *_strpbrk(char *s, char *accept);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *_strstr(char *haystack, char *needle);
+
 
 #endif
