8b8606e4bb60cdcec279c017b53a7b3f17e22dd6
elkomysara <sa1244@fayoum.edu.eg>
2024-06-17 17:39:24 +0300

    alx-low_level_programming/0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,15 +4,12 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-char *string_toupper(char *);
-char *cap_string(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 
 #endif
