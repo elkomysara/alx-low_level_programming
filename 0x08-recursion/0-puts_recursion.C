533c56dc7dc99acaa58946df3331420e251c4dbc
elkomysara <sa1244@fayoum.edu.eg>
2024-06-20 05:48:51 +0300

    alx-low_level_programming/0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.C
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string followed by a new line.
+* @s: The string to print.
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0') /* Base case: if we reach the end of the string */
+{
+_putchar('\n'); /* Print a new line */
+return;
+}
+
+_putchar(*s); /* Print the current character */
+_puts_recursion(s + 1); /* Recursively call the function with the next character */
+}
---
+++ 0x08-recursion/main.c
@@ -0,0 +1,6 @@
+int print(int nb);
+int main(void)
+{
+    print(4);
+    return (0);
+}
