48905b5b78816c04aa74b2ee46fb17b4871abd89
elkomysara <sa1244@fayoum.edu.eg>
2024-06-22 15:28:06 +0300

    alx-low_level_programming/0x08-recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,11 +3,10 @@
 
 #include <stdio.h>
 
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
-
+int _pow_recursion(int x, int y);
 #endif
