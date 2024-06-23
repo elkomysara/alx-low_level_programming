d1d6eaa16cf1dd8a89566c6a97f68c710ffa777e
elkomysara <sa1244@fayoum.edu.eg>
2024-06-23 11:52:56 +0300

    alx-low_level_programming/0x08-recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define FILE_MAIN
 
 #include <stdio.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,4 +12,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
