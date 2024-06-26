2ccca90e420ddb497b5e6f54a023751b2ba28176
elkomysara <sa1244@fayoum.edu.eg>
2024-06-26 13:59:05 +0300

    alx-low_level_programming/0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,10 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-int _putchar(char x);
-int _islower(int x);
-int _isalpha(int x);
-int _abs(int);
+int _putchar(char c);
 int _isupper(int c);
-
+int _isdigit(int c);
 
 #endif
