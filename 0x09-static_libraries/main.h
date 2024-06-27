84e3290d434f1a5264a23ffea9fe4fd9a88c92ab
elkomysara <sa1244@fayoum.edu.eg>
2024-06-28 02:55:38 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -3,27 +3,10 @@
 
 #include <stdio.h>
 
-
-int _putchar(char x);
-int _islower(int x);
-int _isalpha(int x);
-int _abs(int);
-int _isupper(int c);
-int _isdigit(int c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void _puts(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_memset(char *s, char b, unsigned int n);
-char *_strstr(char *haystack, char *needle);
-char *_strpbrk(char *s, char *accept);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strncat(char *dest, char *src, int n);
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 
 #endif
