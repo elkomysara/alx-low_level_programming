221d4b7806a560c0dca36c69dc8ae6c6bc4b6d04
elkomysara <sa1244@fayoum.edu.eg>
2024-06-27 23:28:20 +0300

    alx-low_level_programming/0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,7 +2,6 @@
 #define FILE_MAIN
 
 #include <stdio.h>
-
 int _putchar(char x);
 int _islower(int x);
 int _isalpha(int x);
@@ -10,10 +9,7 @@ int _abs(int);
 int _isupper(int c);
 int _isdigit(int c);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void _puts(char *str);
+
 
 
 #endif
