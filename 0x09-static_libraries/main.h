eccabebe5f4620aa495f7388ee934bdf483e828c
elkomysara <sa1244@fayoum.edu.eg>
2024-06-28 03:18:44 +0300

    alx-low_level_programming/0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -17,6 +17,6 @@ void _puts(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_strstr(char *haystack, char *needle);
-
+char *_strpbrk(char *s, char *accept);
 
 #endif
