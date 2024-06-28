d422ce8d93b8cd077db464ca3da9d15565e021d5
elkomysara <sa1244@fayoum.edu.eg>
2024-06-28 03:18:34 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x09-static_libraries/3-main.c
+++ 0x09-static_libraries/3-main.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
- */
+*/
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+char *str;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (0);
 }
