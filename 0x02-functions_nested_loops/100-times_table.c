cf7ea6324da753c6720fc4c9ddbe4521359ffe6e
elkomysara <sa1244@fayoum.edu.eg>
2024-07-03 10:22:25 +0300

    alx-low_level_programming/0x04-more_functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,33 +1,32 @@
-
 #include <stdio.h>
-#include "main.h"
+
 /**
-* print_times_table - prints the n times table, starting with 0
-* @n: number of the times table
+* print_times_table - prints the n times table, starting with 0.
+* @n: the number of the times table
 */
 void print_times_table(int n)
 {
-int i, j, k;
+int i, j, product;
 
-if (n >= 0 && n <= 15)
+if (n < 0 || n > 15)
 {
+return;
+}
+
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-k = j * i;
+product = i * j;
 if (j == 0)
 {
-printf("%d", k);
+printf("%d", product);
 }
 else
 {
-printf(",%4d", k);
+printf(",%4d", product);
 }
 }
 printf("\n");
 }
-printf("\n");
 }
-}
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,19 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-int _putchar(char x);
-int _islower(int x);
-int _isalpha(int x);
-int _abs(int);
+int _putchar(char c);
 int _isupper(int c);
-
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
