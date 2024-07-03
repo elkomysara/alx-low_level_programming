abd2df08fa2f90987d93fd528d2d2e902b4eae45
elkomysara <sa1244@fayoum.edu.eg>
2024-07-03 09:50:10 +0300

    0x02

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,12 @@ int _islower(int x);
 int _isalpha(int x);
 int print_sign(int x);
 int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+int main(void);
+
 #endif
