d2dc9ee367570e3384c2aba5a0e408e40c692d33
elkomysara <sa1244@fayoum.edu.eg>
2024-07-11 12:06:40 +0300

    alx-low_level_programming/0x10-variadic_functions/0-putchar

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,18 @@
 #include "variadic_functions.h"
 #include <stddef.h>
+#include <unistd.h> /* For write function */
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
 
 /**
 * print_number - prints an integer using _putchar
