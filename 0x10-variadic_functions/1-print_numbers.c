91ea13cc21ed6ca6636ff0fbabd7e4fab9ef0554
elkomysara <sa1244@fayoum.edu.eg>
2024-08-13 00:24:46 +0300

    alx-low_level_programming/0x10-variadic_functions/0-putchar

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers, followed by a new line
+* @separator: the string to be printed between numbers
+* @n: the number of integers passed to the function
+*
+* Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int number;
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+{
+number = va_arg(args, int);
+printf("%d", number);
+if (separator != NULL && i < n - 1)
+{
+printf("%s", separator);
+}
+}
+printf("\n");
+
+va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,10 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stddef.h>
 
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
-
