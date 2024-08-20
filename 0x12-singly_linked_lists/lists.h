ed0fd72a671391f8d2f8a44ef86edc651db166ab
elkomysara <sa1244@fayoum.edu.eg>
2024-08-20 03:01:30 +0300

    alx-low_level_programming/0x12-singly_linked_lists/0-putchar

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,18 +4,21 @@
 #include <stddef.h>
 
 /**
-* struct list_s - singly linked list
-* @str: string - (malloc'd string)
-* @len: length of the string
-* @next: points to the next node
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
