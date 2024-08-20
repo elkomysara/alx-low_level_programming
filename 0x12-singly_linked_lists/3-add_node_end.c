950d517f02e162a17e1c6be645d19e9ec09a0974
elkomysara <sa1244@fayoum.edu.eg>
2024-08-20 10:19:02 +0300

    alx-low_level_programming/0x12-singly_linked_lists/0-putchar

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,16 +3,15 @@
 #include "lists.h"
 
 /**
-* add_node_end - adds a new node at the end of a list_t list
-* @head: double pointer to the head of the list
-* @str: string to be duplicated and added to the new node
-* 
-* Return: the address of the new element, or NULL if it failed
+* add_node_end - Adds a new node at the end of a list_t list.
+* @head: Double pointer to the head of the list_t list.
+* @str: String to be added to the new node.
+*
+* Return: The address of the new element, or NULL if it failed.
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new_node;
-list_t *temp = *head;
+list_t *new_node, *temp;
 
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
@@ -24,6 +23,7 @@ if (new_node->str == NULL)
 free(new_node);
 return (NULL);
 }
+
 new_node->len = strlen(str);
 new_node->next = NULL;
 
@@ -33,6 +33,7 @@ if (*head == NULL)
 }
 else
 {
+temp = *head;
 while (temp->next != NULL)
 temp = temp->next;
 temp->next = new_node;
