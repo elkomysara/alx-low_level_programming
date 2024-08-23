bcc9c27a1fb61e28ac91a4b98d344181ec3102fc
elkomysara <sa1244@fayoum.edu.eg>
2024-08-23 23:01:33 +0300

    alx-low_level_programming/0x15-file_io/0-putchar

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,22 +1,76 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
 
 /**
 * main - Entry point for the program to copy files.
-* @argc: The argument count.
-* @argv: The argument vector.
+* @argc: The argument count
+* @argv: The argument vector
 *
-* Return: 0 on success, or exits with an error code on failure.
+* Return: 0 on success, or exits with an error code on failure
 */
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
+int fd_from, fd_to, r_count, w_count;
+char buffer[BUFFER_SIZE];
 
-check_args(argc);
-fd_from = open_file_from(argv[1]);
-fd_to = open_file_to(argv[2]);
-copy_content(fd_from, fd_to);
-close_file(fd_from);
-close_file(fd_to);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(fd_from);
+exit(99);
+}
+
+while ((r_count = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+w_count = write(fd_to, buffer, r_count);
+if (w_count != r_count)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(fd_from);
+close(fd_to);
+exit(99);
+}
+}
+
+if (r_count == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close(fd_from);
+close(fd_to);
+exit(98);
+}
+
+if (close(fd_from) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+close(fd_to);
+exit(100);
+}
+
+if (close(fd_to) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+exit(100);
+}
 
 return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,11 +7,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-
-void check_args(int argc);
-int open_file_from(const char *filename);
-int open_file_to(const char *filename);
-void copy_content(int fd_from, int fd_to);
-void close_file(int fd);
+int main(int argc, char *argv[]);
 #endif /* MAIN_H */
