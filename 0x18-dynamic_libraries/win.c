1c4136bf1b82048fee14bd2fad8b9ba17c5288db
elkomysara <sa1244@fayoum.edu.eg>
2024-09-04 13:30:48 +0300

    alx-low_level_programming/0x18-dynamic_libraries/1-dlistint_len

--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -3,13 +3,13 @@
 
 int rand(void)
 {
-    static int i = -1;
-    i++;
-    if (i == 0) return 9;
-    if (i == 1) return 8;
-    if (i == 2) return 10;
-    if (i == 3) return 24;
-    if (i == 4) return 75;
-    if (i == 5) return 9;
-    return rand();  // Default behavior for any other calls
+static int counter = 0;
+counter++;
+if (counter == 1) return 9;   
+if (counter == 2) return 8;  
+if (counter == 3) return 10;  
+if (counter == 4) return 24;  
+if (counter == 5) return 75;  
+if (counter == 6) return 9;   
+return 0;  
 }
