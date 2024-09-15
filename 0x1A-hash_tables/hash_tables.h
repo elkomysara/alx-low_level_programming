4bffaf5815519cf0d2fb3ae305e8354e8234f43f
elkomysara <sa1244@fayoum.edu.eg>
2024-09-15 21:17:44 +0300

    Implement key_index function using hash_djb2

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,15 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
 * struct hash_node_s - Node of a hash table
 * @key: The key, string
 * @value: The value corresponding to a key
-* @next: A pointer to the next node of the list
+* @next: A pointer to the next node of the List
 */
 typedef struct hash_node_s
 {
@@ -25,7 +29,9 @@ unsigned long int size;
 hash_node_t **array;
 } hash_table_t;
 
+/* Function Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif /* HASH_TABLES_H */
