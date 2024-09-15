1e9236e0fe3cfe7419aace2db7f3a750df6a62b2
elkomysara <sa1244@fayoum.edu.eg>
2024-09-15 21:24:14 +0300

    Implement hash_table_set function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,13 +1,15 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
 * struct hash_node_s - Node of a hash table
+*
 * @key: The key, string
+* The key is unique in the HashTable
 * @value: The value corresponding to a key
 * @next: A pointer to the next node of the List
 */
@@ -20,8 +22,11 @@ struct hash_node_s *next;
 
 /**
 * struct hash_table_s - Hash table data structure
+*
 * @size: The size of the array
 * @array: An array of size @size
+* Each cell of this array is a pointer to the first node of a linked list,
+* because we want our HashTable to use a Chaining collision handling
 */
 typedef struct hash_table_s
 {
@@ -29,9 +34,9 @@ unsigned long int size;
 hash_node_t **array;
 } hash_table_t;
 
-/* Function Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif /* HASH_TABLES_H */
